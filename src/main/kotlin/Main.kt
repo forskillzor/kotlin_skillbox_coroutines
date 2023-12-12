9866bfbd4b5f77573c877b397fe25fe48b2c6417
Sergey Orlov <formyfrontend@gmail.com>
2023-12-12 15:26:10 +0700

    check

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,37 @@
-fun main() {
-    println("Hello Elena!")
+import java.math.BigInteger
+import kotlinx.coroutines.*
+
+fun fibonacci(n: Int): BigInteger {
+    if (n <= 1) {
+        return BigInteger.valueOf(n.toLong())
+    }
+
+    var fibMinusTwo = BigInteger.ZERO
+    var fibMinusOne = BigInteger.ONE
+
+    for (i in 2..n) {
+        val currentFib = fibMinusTwo + fibMinusOne
+        fibMinusTwo = fibMinusOne
+        fibMinusOne = currentFib
+    }
+
+    return fibMinusOne
+}
+
+object Fibonacci {
+    suspend fun take(n: Int): BigInteger {
+        return fibonacci(n)
+    }
+}
+
+suspend fun main() {
+    runBlocking {
+        launch { println(Fibonacci.take(5)) }
+        launch { println(Fibonacci.take(10)) }
+        launch { println(Fibonacci.take(15)) }
+
+        launch { println(Fibonacci.take(35)) }
+        launch { println(Fibonacci.take(30)) }
+        launch { println(Fibonacci.take(25)) }
+    }
 }
